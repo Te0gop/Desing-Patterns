88eb769dec9146b5c94593fa374213f5bc65428e
Teodor <te0gop.u@gmail.com>
2022-01-04 22:06:25 +0200

    creational design patterns

--- Creational-Patterns/src/prototype/Item.java
+++ Creational-Patterns/src/prototype/Item.java
@@ -13,18 +13,23 @@ public abstract class Item implements Cloneable {
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
