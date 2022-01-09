1421249de1f134df86e9d7f9ef7669f9a083b5dd
Teodor <te0gop.u@gmail.com>
2022-01-09 11:25:16 +0200

    flyweight pattern

---
+++ Structural-Patterns/src/flyweight/FlyweightInventoryDemo.java
@@ -0,0 +1,28 @@
+package flyweight;
+
+public class FlyweightInventoryDemo {
+
+	public static void main(String[] args) {
+		InventorySystem ims = new InventorySystem();
+
+		ims.takeOrder("Roomba", 221);
+		ims.takeOrder("Bose Headphones", 361);
+		ims.takeOrder("Samsung TV", 432);
+		ims.takeOrder("Samsung TV", 323);
+		ims.takeOrder("Roomba", 563);
+		ims.takeOrder("Bose Headphones", 321);
+		ims.takeOrder("Roomba", 234);
+		ims.takeOrder("Samsung TV", 54);
+		ims.takeOrder("Roomba", 34);
+		ims.takeOrder("Bose Headphones", 365);
+		ims.takeOrder("Samsung TV", 332);
+		ims.takeOrder("Roomba", 456);
+
+		ims.process(); 
+
+		// We could take a million orders but, we will create only 3 objects. That is very big performance boost for our system.
+		// This pattern is used in places like Amazon, Walmart etc.
+		System.out.println(ims.report());
+
+	}
+}
