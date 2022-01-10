283d5111766227936bf839d3e5f864771fb1a48a
Teodor <te0gop.u@gmail.com>
2022-01-10 15:31:51 +0200

    chain of responsibillity pattern

--- Behavioral-Patterns/src/main/java/chain/ChainOfResponsibilityEverydayDemo.java
+++ Behavioral-Patterns/src/main/java/chain/ChainOfResponsibilityEverydayDemo.java
@@ -4,7 +4,6 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class ChainOfResponsibilityEverydayDemo {
 
 	private static final Logger logger = Logger.getLogger(ChainOfResponsibilityEverydayDemo.class.getName());
