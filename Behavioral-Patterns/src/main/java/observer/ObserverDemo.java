5603841573364ee4c411094e7d06ce5d7ebef829
Teodor <te0gop.u@gmail.com>
2022-01-11 11:50:01 +0200

    observer pattern

--- Behavioral-Patterns/src/main/java/observer/ObserverDemo.java
+++ Behavioral-Patterns/src/main/java/observer/ObserverDemo.java
@@ -2,7 +2,7 @@ package observer;
 
 public class ObserverDemo {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		Subject subject = new MessageStream();
 		
 		PhoneClient phoneClient = new PhoneClient(subject);
